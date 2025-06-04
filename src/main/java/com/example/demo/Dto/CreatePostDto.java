30d3e1aaae985fd16dcd944dc4c1352c4be6e436
ast-shoji-001 <yuki.shoji@synchrotech.co.jp>
2025-06-04 17:04:24 +0900

    Postエンティティの実装

---
+++ src/main/java/com/example/demo/Dto/CreatePostDto.java
@@ -0,0 +1,21 @@
+package com.example.demo.Dto;
+
+import java.util.List;
+
+import lombok.Getter;
+
+@Getter
+public class CreatePostDto {
+	
+	private Long userId;
+	private String text;
+	private Integer feelingScore;
+	private List<String> emotionKeywords;
+	
+	public CreatePostDto(Long userId, String text, Integer feelingScore, List<String> emotionKeywords) {
+		this.userId = userId;
+		this.text = text;
+		this.feelingScore = feelingScore;
+		this.emotionKeywords = emotionKeywords;
+	}
+}
---
+++ src/main/java/com/example/demo/entity/Post.java
@@ -0,0 +1,41 @@
+package com.example.demo.entity;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name="post")
+public class Post extends BaseEntity {
+	private Long userId;
+	private String text;
+	private Integer feelingScore;
+	private String mode;
+	private List<String> emotionKeywords;
+	private Boolean isVisible;
+	private LocalDateTime createdAt;
+	
+	public Post() {}
+	
+	public Post(String text, Integer feelingScore, String mode, List<String> emotionKeywords, Boolean isVisible, LocalDateTime createdAt) {
+		this.text = text;
+		this.feelingScore = feelingScore;
+		this.mode = mode;
+		this.emotionKeywords = emotionKeywords;
+		this.isVisible = isVisible;
+		this.createdAt = createdAt;
+	}
+	
+	@Override
+	public String toString() {
+		return super.toString() + "";
+	}
+	
+}
