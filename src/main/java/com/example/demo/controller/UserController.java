d7c056ff0c448a44db0c54d1444da6c5b41639e7
ast-shoji-001 <yuki.shoji@synchrotech.co.jp>
2025-06-04 11:54:21 +0900

    UserServiceの実装

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,20 +1,38 @@
 package com.example.demo.controller;
 
-import java.time.LocalDateTime;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.User;
+import com.example.demo.service.UserService;
 
 @RestController
 @RequestMapping("/api")
 public class UserController {
 	
+	private final UserService service;
+	
+	@Autowired
+	  public UserController(UserService service) {
+	    this.service = service;
+	  }
+	
 	@GetMapping("/users")
-	private String getUsers() {
-		User user = new User("shoji","sample.com","shoji1234","庄司",LocalDateTime.now());
-		return user.toString();
+	public ResponseEntity<List<User>> getUsers() {
+		return ResponseEntity.ok(service.getUsers());
 	}
+	
+	@PostMapping("/users")
+	public ResponseEntity<User> registUser(@RequestBody User user) {
+		return ResponseEntity.ok(service.registUser(user));
+	}
+	
 }
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,8 +1,42 @@
 package com.example.demo.service;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.User;
+import com.example.demo.utility.PasswordEncoder;
 
 @Service
 public class UserService {
+	
+	public List<User> getUsers() {
+		// TODO データベースへ格納し、リポジトリから呼び出し
+		List<User> users = new ArrayList<>();
 
+        users.add(new User("田中一郎", "tanaka@example.com", "hashed_pw_001", "いっちゃん", LocalDateTime.of(2023, 1, 15, 10, 30)));
+        users.add(new User("鈴木花子", "suzuki@example.com", "hashed_pw_002", "ハナ", LocalDateTime.of(2023, 2, 20, 14, 10)));
+        users.add(new User("佐藤次郎", "sato@example.com", "hashed_pw_003", "ジロ", LocalDateTime.of(2023, 3, 25, 9, 0)));
+        users.add(new User("山田太郎", "yamada@example.com", "hashed_pw_004", "たろちゃん", LocalDateTime.of(2024, 5, 1, 16, 45)));
+        users.add(new User("小林美咲", "kobayashi@example.com", "hashed_pw_005", "ミサ", LocalDateTime.of(2024, 11, 3, 8, 20)));
+
+        return users;
+	}
+	
+	
+	public User registUser(User user) {
+	
+		User registUser = new User(
+				user.getName(), 
+				user.getEmail(), 
+				PasswordEncoder.hash(user.getPassword()), 
+				user.getNickname(), 
+				LocalDateTime.now());
+		
+		//TODO リポジトリへ保存処理の記述
+		
+		return registUser;
+	}
 }
